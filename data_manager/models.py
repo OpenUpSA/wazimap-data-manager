957eccfe66932620156bcf885824740aa1a9e529
lunga <stumenz.complex@gmail.com>
2019-12-06 13:02:18 +0200

    add models to remove parsing of filefield

--- data_manager/models.py
+++ data_manager/models.py
@@ -2,3 +2,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from wazimap.models import FieldTable
+
+
+class UploadedDataset(models.Model):
+    field_table = models.ForeignKey(FieldTable, on_delete=models.CASCADE)
+    dataset_file = models.FileField(upload_to="datasets/")
